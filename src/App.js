59738076b3fb1b9a91e32c2848d367d5e259c10f
labbeltran <labbeltran@gmail.com>
2024-02-15 20:38:39 -0500

    deleted Posting and Help from app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 // Import data
-import React, { useState } from 'react'
+import React from 'react'
 // Import components
 import './App.css';
 import Searchbar from './Components/Searchbar.js'
 import Directory from './Components/Directory.js'
 import Gallery from './Components/Gallery.js'
 import Sidebar from './Components/Sidebar.js'
-import Help from './Components/Help.js'
+
 
 
 function App(){
   return (
    <div className="App">
       <Searchbar/>
-      <Directory/>
       <Gallery/>
-      <Posting/>
       <Sidebar/>
-      <Help/>
+      <Directory/>
    </div>
   );
 }
--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,14 +1,19 @@
-import React, { useState } from 'react'
+// Gallery.js
+import React from 'react';
+import { postings } from '../path/to/postings'; // Adjust the import path as necessary
+import Posting from './Component/Posting'; // Adjust the path to where your Posting component is located
 
-function Gallery(){
+function Gallery() {
+  const postList = postings.map((posting, index) => (
+    <Posting key={index} {...posting} />
+  ));
 
-    return(
+  return (
     <div className="gallery">
       <h1>Gallery</h1>
-      <Posting/>
+      {postList}
     </div>
-      
-    );
-  };
+  );
+}
 
-  export default Gallery;
+export default Gallery;
